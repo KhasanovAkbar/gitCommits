cfbd0630db7d123c09b3f6dd06f2412f87ba21d5
KhasanovAkbar <akbar.hasanov0630@gmail.om>
2023-12-14 22:56:47 +0100

    created readme

---
+++ src/test/java/com/epam/gitcommits/GitCommitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.gitcommits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCommitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
